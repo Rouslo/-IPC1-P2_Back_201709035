1b3502ff84f7cdc47403ac48ff108d4a95f684e7
-IPC1-P2_Front_201709035 <rlopez2014ig@gmail.com>
2020-10-24 22:04:59 -0600

    modified

--- index.py
+++ index.py
@@ -1,30 +1,40 @@
   
 from flask import Flask, render_template, request, redirect, url_for, flash, current_app
-import json
+from flask import Markup 
 name=""
 contraseña=""
 app = Flask(__name__)
 
+
 @app.route('/')
 def main_page():
-    current_app.logger.info('Mostrando los posts del blog')
-    
-    return None
+    return render_template('principal.html')
 
-
-@app.route('/inicio.html',methods=["GET", "POST"])
+@app.route('/insertar',methods=["GET", "POST"])
 def add_contact():
     if request.method=='POST':
-        name=request.form['name']
-        contraseña=request.form['contraseña']
+        name=request.form['u1']
+        contraseña=request.form['u2']
         print(name)
         print(contraseña)
-             
-        #return render_template('index.html')
-        
-        
+       
+        flash("REGISTRADO") 
+        return render_template('registro.html') 
+
+@app.route('/ir_registro',methods=["GET", "POST"])
+def ir_registro():
+    if request.method=='POST':
+        return render_template('registro.html') 
+
+@app.route('/ir_login',methods=["GET", "POST"])
+def ir_login():
+    if request.method=='POST':
+        return render_template('login.html') 
+    
 
 if __name__ == '__main__':
-     app.run()
+    app.secret_key = 'super secret key' 
+    app.config['SESSION_TYPE'] = 'filesystem' 
+    app.run()
         
 

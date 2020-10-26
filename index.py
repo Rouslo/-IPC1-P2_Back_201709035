1359a1dab5e74fa77ce46b8a4ee3eedcb2b54da6
-IPC1-P2_Front_201709035 <rlopez2014ig@gmail.com>
2020-10-25 19:00:55 -0600

    sigo modificando

--- index.py
+++ index.py
@@ -4,22 +4,79 @@ from flask import Markup
 name=""
 contraseña=""
 app = Flask(__name__)
+global cliente
+cliente=[]
+usu="admin"
+ape="Maestro"
+nom="Usuario"
+contra="admin"
 
 
 @app.route('/')
 def main_page():
-    return render_template('principal.html')
+    cliente.append(["Usuario","Maestro","admin","admin"])
+    return render_template('admo.html')
 
 @app.route('/insertar',methods=["GET", "POST"])
 def add_contact():
+    estado=False
     if request.method=='POST':
-        name=request.form['u1']
-        contraseña=request.form['u2']
-        print(name)
-        print(contraseña)
-       
-        flash("REGISTRADO") 
-        return render_template('registro.html') 
+        a=request.form['u1']
+        b=request.form['u2']
+        c=request.form['u3']
+        d=request.form['p1']
+        for i in range(len(cliente)):
+            if cliente[i][2]==str(c):
+                estado=True
+
+        if(estado==False):
+            cliente.append([str(a),str(b),str(c),str(d)])
+            flash("REGISTRADO") 
+            return render_template('principal.html') 
+        else:
+            flash("REGISTRO YA EXISTE") 
+            return render_template('registro.html')
+
+
+@app.route('/leer',methods=["GET", "POST"])
+def ver_login():
+    estado=False
+    if request.method=='POST':
+        a=request.form['u1']
+        b=request.form['p1']
+        for i in range(len(cliente)):
+            if cliente[i][2]==a and cliente[i][3]==b:
+                estado=True
+        if(a==usu and b==contra):
+            flash("BIENVENIDO ADMO") 
+            return render_template('principal.html')
+        
+        else:
+            if(estado==False):
+                flash("USUARIO NO EXISTE") 
+                return render_template('principal.html') 
+            else:
+                flash("BIENVENIDO") 
+                return render_template('login.html') 
+
+@app.route('/recuperar',methods=["GET", "POST"])
+def recuperar():
+    if request.method=='POST':
+        a=request.form['u1']
+        for i in range(len(cliente)):
+            if cliente[i][2]==a:
+                flash("su contraseña es: "+cliente[i][3]) 
+                return render_template('login.html') 
+ 
+            else:
+                flash("USUARIO NO EXISTE") 
+                return render_template('principal.html') 
+        
+
+
+
+
+
 
 @app.route('/ir_registro',methods=["GET", "POST"])
 def ir_registro():
@@ -30,6 +87,11 @@ def ir_registro():
 def ir_login():
     if request.method=='POST':
         return render_template('login.html') 
+
+@app.route('/ir_recuperar',methods=["GET", "POST"])
+def ir_recuperar():
+    if request.method=='POST':
+        return render_template('recuperar.html') 
     
 
 if __name__ == '__main__':
@@ -37,4 +99,6 @@ if __name__ == '__main__':
     app.config['SESSION_TYPE'] = 'filesystem' 
     app.run()
         
+
+
 

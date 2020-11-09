47eda98297f3c999aa1e6b04e6944a98f3e9a605
-IPC1-P2_Front_201709035 <rlopez2014ig@gmail.com>
2020-11-09 17:23:23 -0600

    mod

--- index.py
+++ index.py
@@ -12,24 +12,12 @@ global cliente
 cliente=[]
 global aplicaciones
 aplicaciones=[]
-global adm
-adm=[]
 
 usu="admin"
 ape="Maestro"
 nom="Usuario"
 contra="admin"
 
-
-
-11->12->13->14
-21->22->23->24
-31->32->33->34
-41->42->43->44 c y f    me da la seguda mano
-
-
-
-# --------------------------------------------------------------------------------principales-------------
 
 @app.route('/')
 def main_page():
@@ -56,51 +44,13 @@ def add_contact():
             flash("REGISTRO YA EXISTE") 
             return render_template('registro.html')
 
-@app.route('/guardar_admo',methods=["GET", "POST"])
-def agregaradmo():
-    estado=False
-    if request.method=='POST':
-        a=request.form['u1']
-        b=request.form['u2']
-        c=request.form['u3']
-        d=request.form['u4']
-        for i in range(len(adm)):
-            if adm[i][2]==str(c):
-                estado=True
-
-        if(estado==False):
-            adm.append([str(a),str(b),str(c),str(d)])
-            flash("REGISTRADO") 
-            return render_template('admo.html') 
-        else:
-            flash("REGISTRO YA EXISTE") 
-            return render_template('admo.html')
-
 
 @app.route('/leer',methods=["GET", "POST"])
 def ver_login():
     estado=False
-    estado1=False
-    print("mama")
     if request.method=='POST':
         a=request.form['u1']
         b=request.form['p1']
-        z=request.form['op']
-        print(z)
-        
-        if(z=="ADMO"):
-            for i in range(len(adm)):
-                if adm[i][2]==a and adm[i][3]==b:
-                    estado1=True
-
-            if(estado1==False):
-                flash("USUARIO NO EXISTE") 
-                return render_template('principal.html') 
-            else:
-                flash("BIENVENIDO ADMO") 
-                return render_template('admo.html')
-        
-        else:
         for i in range(len(cliente)):
             if cliente[i][2]==a and cliente[i][3]==b:
                 estado=True
@@ -143,19 +93,6 @@ def carga():
     print(aplicaciones)
     return render_template('admo.html')
 
-@app.route('/cargacliente',methods=["GET", "POST"])
-def cargacliente():
-    if request.method=='POST':
-        a=request.form['file']
-        with open(""+a) as File:
-            reader = csv.reader(File, delimiter=',', quotechar=',',quoting=csv.QUOTE_MINIMAL)
-            for row in reader:
-                cliente.append(row)  
-
-    flash("CARGA EXITOSA")
-    return render_template('admo.html')
-
-
 @app.route('/pdf1',methods=["GET", "POST"])
 def pdf1():
     if request.method=='POST':
@@ -163,18 +100,6 @@ def pdf1():
         mama=0
         for i in range(len(aplicaciones)):
             p.drawString(5, mama, ""+str(aplicaciones[i]))
-            mama=mama+30
-        p.showPage()
-        p.save()
-    return render_template('admo.html') 
-
-@app.route('/pdf2',methods=["GET", "POST"])
-def pdf2():
-    if request.method=='POST':
-        p = canvas.Canvas("clientes.pdf")
-        mama=0
-        for i in range(len(cliente)):
-            p.drawString(5, mama, ""+str(cliente[i]))
             mama=mama+30
         p.showPage()
         p.save()
@@ -223,43 +148,14 @@ def modificar_aplicacion():
     return render_template('admo.html')
 
 
-@app.route('/reseña/<string:id>', methods = ['POST','GET'])
-def rese(id):
-    y=0
-    for i in range(len(aplicaciones)):
-        if aplicaciones[i][0]==str(id):
-            y=i
-            break
-
-    return render_template('cliente2.html', contact = aplicaciones[y])
-
-
-@app.route('/insertar_reseña',methods=["GET", "POST"])
-def insereseña():
-    if request.method=='POST':
-        a=request.form['u1']
-    reseñas.append([str(a)])
-    flash('RESEÑA AGREGADA')
-    return render_template('cliente1.html')
-
-
-
-@app.route('/likes/<string:id>', methods = ['POST','GET'])
-def likes(id):
-    for i in range(len(aplicaciones)):
-        if aplicaciones[i][0]==str(id):
-            a=int(aplicaciones[i][7])+1
-            aplicaciones.append([aplicaciones[i][0],aplicaciones[i][1],aplicaciones[i][2],aplicaciones[i][3],aplicaciones[i][4],aplicaciones[i][5],aplicaciones[i][6],str(a)])
-            aplicaciones.pop(i)
-            break
 
 
 
 
-# --------------------------------------------------------------------------------ir a paginas-------------
-# --------------------------------------------------------------------------------ir a paginas-------------
-# --------------------------------------------------------------------------------ir a paginas-------------
-# --------------------------------------------------------------------------------ir a paginas-------------
+
+
+
+
 
 @app.route('/ir_registro',methods=["GET", "POST"])
 def ir_registro():
@@ -286,24 +182,6 @@ def ir_admo():
 def ir_admo1():
     if request.method=='POST':
         return render_template('admo1.html',contacts=aplicaciones) 
-
-    if request.method=='GET':
-        return render_template('admo1.html',contacts=cliente) 
-
-@app.route('/ir_tabla2',methods=["GET", "POST"])
-def ir_admo3():
-    if request.method=='POST':
-        return render_template('admo3.html',contacts=cliente) 
-
-@app.route('/iradmo4',methods=["GET", "POST"])
-def ir_admo4():
-    if request.method=='POST':
-        return render_template('admo4.html') 
-
-@app.route('/irinicio',methods=["GET", "POST"])
-def cerrar():
-    if request.method=='POST':
-        return render_template('principal.html') 
 
 
 
